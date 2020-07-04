184ee75e4ebf49f2f813970f00a8ab10ae4cb240
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-07-04 16:00:52 +0200

    refactoring specialize section

--- src/components/specialize-section/specialize-section.component.tsx
+++ src/components/specialize-section/specialize-section.component.tsx
@@ -1,8 +1,70 @@
 import React, { FunctionComponent } from 'react';
 import './specialize-section.styles.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDesktop, faUser } from '@fortawesome/free-solid-svg-icons';
+import {
+  faDesktop,
+  faUser,
+  IconDefinition,
+} from '@fortawesome/free-solid-svg-icons';
 import { faReact, faSass } from '@fortawesome/free-brands-svg-icons';
+
+interface SpecializeBoxParams {
+  icon: IconDefinition;
+  title: string;
+  description: string;
+}
+
+const SpecializeBox: FunctionComponent<SpecializeBoxParams> = ({
+  icon,
+  title,
+  description,
+}) => {
+  return (
+    <div className="specialization-box">
+      <span className="fa-icon">
+        <FontAwesomeIcon icon={icon} size="2x" />
+      </span>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+const specializations: SpecializeBoxParams[] = [
+  {
+    icon: faDesktop,
+    title: 'Demo',
+    description: `
+    I am a demo, or better a simple project made for practicing/demo
+    purposes. Please do not take me seriously.
+    `,
+  },
+  {
+    icon: faUser,
+    title: 'Portfolio',
+    description: `
+    More specifically I am a demo of a portfolio website, so you can
+    kind of pretent that I am presenting the accomplishments or the
+    like of someone.
+    `,
+  },
+  {
+    icon: faReact,
+    title: 'React',
+    description: `
+    I am implemented in react (a very simple implementation with only
+    presentational components) and coded in typescript.
+    `,
+  },
+  {
+    icon: faSass,
+    title: 'Sass',
+    description: `
+    My styles have been written in sass, which is a css precompiler.
+    The actual css is generated on build and used for deployment.
+    `,
+  },
+];
 
 const SpecializeSection: FunctionComponent = () => {
   return (
@@ -15,47 +77,9 @@ const SpecializeSection: FunctionComponent = () => {
           using React, SASS and Typescript
         </p>
         <div className="specializations">
-          <div>
-            <span className="fa-icon">
-              <FontAwesomeIcon icon={faDesktop} size="2x" />
-            </span>
-            <h3>Demo</h3>
-            <p>
-              I am a demo, or better a simple project made for practicing/demo
-              purposes. Please do not take me seriously.
-            </p>
-          </div>
-          <div>
-            <span className="fa-icon">
-              <FontAwesomeIcon icon={faUser} size="2x" />
-            </span>
-            <h3>Portfolio</h3>
-            <p>
-              More specifically I am a demo of a portfolio website, so you can
-              kind of pretent that I am presenting the accomplishments or the
-              like of someone.
-            </p>
-          </div>
-          <div>
-            <span className="fa-icon">
-              <FontAwesomeIcon icon={faReact} size="2x" />
-            </span>
-            <h3>React</h3>
-            <p>
-              I am implemented in react (a very simple implementation with only
-              presentational components) and coded in typescript.
-            </p>
-          </div>
-          <div>
-            <span className="fa-icon">
-              <FontAwesomeIcon icon={faSass} size="2x" />
-            </span>
-            <h3>Sass</h3>
-            <p>
-              My styles have been written in sass, which is a css precompiler.
-              The actual css is generated on build and used for deployment.
-            </p>
-          </div>
+          {specializations.map((s, idx) => (
+            <SpecializeBox key={idx} {...s} />
+          ))}
         </div>
       </div>
     </section>
