5462cb6ef0ad68e19b4ffae7df8e3cbda0d26494
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-07-04 23:05:51 +0200

    made footer's github icon a link to the github repo

--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -3,13 +3,19 @@ import './footer.styles.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
+const githubBaseUrl = 'https://github.com/dario-piotrowicz/';
+const githubRepoName = 'ReactJS-SCSS_demo_portfolio_website_miniproject';
+const githubRepoUrl = `${githubBaseUrl}${githubRepoName}`;
+
 const Footer: FunctionComponent = () => {
   return (
     <footer id="main-footer">
       <div className="centering-container">
         <p>Copyright &copy; 2020. All rights Reserved</p>
         <span className="fa-icon">
-          <FontAwesomeIcon icon={faGithub} size="2x" />
+          <a href={githubRepoUrl} target="_blank">
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
         </span>
       </div>
     </footer>
