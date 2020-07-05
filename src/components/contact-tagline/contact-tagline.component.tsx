7fa764756e063af84b0c654ea692bef34a5f4a58
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-07-05 21:35:59 +0200

    added contact tagline

---
+++ src/components/contact-tagline/contact-tagline.component.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent } from 'react';
+import './contact-tagline.styles.scss';
+
+const ContactTagline: FunctionComponent = () => {
+  return (
+    <section id="tagline">
+      <div className="centering-container">
+        <h1>Please Leave Me Alone,</h1>
+        <h1>I Am Just a Demo Project!</h1>
+      </div>
+    </section>
+  );
+};
+
+export default ContactTagline;
--- src/pages/contact/contact.page.component.tsx
+++ src/pages/contact/contact.page.component.tsx
@@ -2,12 +2,14 @@ import React, { FunctionComponent } from 'react';
 import './contact.page.styles.scss';
 import ContactForm from '../../components/contact-form/contact-form.component';
 import ContactInfoSection from '../../components/contact-info-section/contact-info-section.component';
+import ContactTagline from '../../components/contact-tagline/contact-tagline.component';
 
 const ContactPage: FunctionComponent = () => {
   return (
     <>
       <ContactForm />
       <ContactInfoSection />
+      <ContactTagline />
     </>
   );
 };
